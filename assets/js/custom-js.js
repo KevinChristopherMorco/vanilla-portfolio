0d2faab1c54409bc6105e23212202b91d1ee00ce
Kevin Christopher Morco <christophermorco02@gmail.com>
2024-05-02 14:41:39 +0800

    updated mobile responsiveness

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -2,18 +2,21 @@
 const imageLoad = () => {
     const heroImage = document.querySelector('.hero__start-img > img')
     const heroMotto = document.querySelectorAll('.hero__container .hero__info .hero__motto-container>p')
+    
     const imagePath = ['pictures/hero/magnifying-glass.png', 'pictures/hero/visualize.png', 'pictures/hero/computer.png']
     const mottoClass = ['first', 'second', 'third']
 
+    //Counter was set to 1 since there is a default html value
     let counter = 1
 
     setInterval(() => {
         heroMotto.forEach((motto, index) => {
-            motto.classList.remove(`hero__motto--focus--${mottoClass[index]}`);
-        });
+            motto.classList.remove(`hero__motto--focus--${mottoClass[index]}`)
+        })
 
         heroImage.setAttribute('src', imagePath[counter])
         heroMotto[counter].classList.add(`hero__motto--focus--${mottoClass[counter]}`)
+
         if (counter === imagePath.length - 1) {
             counter = 0
             return
@@ -29,11 +32,11 @@ imageLoad()
 const hamburgerIcon = document.querySelector('.nav__hamburger-container')
 const hamburgerWrapper = document.querySelector('.hamburger__wrapper')
 const hamburgerContent = document.querySelector('.hamburger__container')
+const closehamburgerWrapper = document.querySelector('.close-hamburger')
+
 const hamburgerOverlay = document.querySelectorAll('.hamburger__overlay-bg')
 
-const closehamburgerWrapper = document.querySelector('.close-hamburger')
 const animation = () => {
-
 
     hamburgerIcon.addEventListener('click', (e) => {
         hamburgerWrapper.classList.add('show')
@@ -61,15 +64,15 @@ const animation = () => {
 }
 animation()
 
-
 //Manipulate theme
 const theme = () => {
     const heroTheme = document.querySelector('.hero__theme-container')
+    const lightTheme = document.querySelector('.light-theme')
+    const darkTheme = document.querySelector('.dark-theme')
+    const brownTheme = document.querySelector('.brown-theme')
+    const blueTheme = document.querySelector('.blue-theme')
 
-    const lightTheme = document.getElementById('light-theme')
-    const darkTheme = document.getElementById('dark-theme')
-    const brownTheme = document.getElementById('brown-theme')
-    const blueTheme = document.getElementById('blue-theme')
+    const logo = document.querySelector('.nav__logo-container > img')
 
     heroTheme.addEventListener('click', (e) => {
         if (e.target.classList.contains('hero__theme')) {
@@ -171,22 +174,27 @@ const theme = () => {
 
     lightTheme.addEventListener('click', (e) => {
         setTheme(lightThemeColor)
+        logo.setAttribute('src', 'pictures/logo/lighttheme-logo.png')
     })
 
     darkTheme.addEventListener('click', (e) => {
         setTheme(darkThemeColor)
+        logo.setAttribute('src', 'pictures/logo/darktheme-logo.png')
+
     })
 
     brownTheme.addEventListener('click', (e) => {
         setTheme(brownThemeColor)
+        logo.setAttribute('src', 'pictures/logo/browntheme-logo.png')
+
     })
 
     blueTheme.addEventListener('click', (e) => {
         setTheme(blueThemeColor)
+        logo.setAttribute('src', 'pictures/logo/bluetheme-logo.png')
     })
 }
 theme()
-
 
 //View Next Page
 const nextSection = document.querySelector('.about__extra-info')
@@ -197,8 +205,12 @@ const header = document.querySelector('.nav__main-container')
 const hamburgerList = document.querySelector('.hamburger__wrapper .hamburger__container .hamburger__content > ul')
 const mainList = document.querySelector('.nav__main-container > ul')
 
+//Next Page and list
 let nextPage = () => {
     nextSection.addEventListener('click', (e) => {
+        const listName = ['Home', 'Skills', 'Stacks', 'Random', 'Contacts']
+        const listId = ['home', 'skills', 'stacks', 'random', 'contact']
+
         mainSection.style.cssText = 'display:none;'
         nextContent.style.cssText = 'display:block;'
         header.style.cssText = 'animation:opacity 2s forwards;'
@@ -206,66 +218,91 @@ let nextPage = () => {
         document.documentElement.scrollTop = 0;
         document.body.scrollTop = 0;
 
-        changeList()
+        while (hamburgerList.firstChild) {
+            hamburgerList.removeChild(hamburgerList.firstChild);
+        }
+
+        while (mainList.firstChild) {
+            mainList.removeChild(mainList.firstChild);
+        }
+
+        for (let i = 0; i < listName.length; i++) {
+            const createList = document.createElement('li')
+            hamburgerList.appendChild(createList)
+
+            const createAnchor = document.createElement('a')
+            createAnchor.href = `#${listId[i]}`
+            createAnchor.textContent = `${listName[i]}`
+            createList.appendChild(createAnchor)
+
+            if (i === 0) {
+                createAnchor.setAttribute('id', 'home')
+                createAnchor.setAttribute('class', 'home')
+            }
+            //Cloned lists for desktops
+            const cloneList = createList.cloneNode(true)
+            mainList.appendChild(cloneList)
+        }
 
     })
 }
 nextPage()
 
+//Previous Page and list
+mainList.addEventListener('click', (e) => {
+    const listName = ['About', 'Projects', 'Contacts']
+    const listId = ['about', 'project', 'contact']
 
-const changeList = () => {
-    while (hamburgerList.firstChild) {
-        hamburgerList.removeChild(hamburgerList.firstChild);
-    }
-
-    while (mainList.firstChild) {
-        mainList.removeChild(mainList.firstChild);
-    }
-
-    const listName = ['Home', 'Skills', 'Stacks', 'Random', 'Contacts']
-    const listId = ['home', 'skills', 'stacks', 'random', 'contact']
-
-    for (let i = 0; i < listName.length; i++) {
-        const createList = document.createElement('li')
-        hamburgerList.appendChild(createList)
-
-        const createAnchor = document.createElement('a')
-        createAnchor.href = `#${listId[i]}`
-        createAnchor.textContent = `${listName[i]}`
-        createList.appendChild(createAnchor)
-
-        if (i === 0) {
-            createAnchor.setAttribute('íd', 'home')
-            createAnchor.addEventListener('click', (e) => {
-                while (mainList.firstChild) {
-                    mainList.removeChild(mainList.firstChild);
-                }
-                mainSection.style.cssText = 'display:block;'
-                nextContent.style.cssText = 'display:none;'
-                header.style.cssText = 'animation:opacity 2s forwards;'
-                const listName = ['About', 'Projects', 'Contacts']
-                const listId = ['about', 'project', 'contact']
-
-                for (let i = 0; i < listName.length; i++) {
-                    const createList = document.createElement('li')
-                    mainList.appendChild(createList)
-
-                    const createAnchor = document.createElement('a')
-                    createAnchor.href = `#${listId[i]}`
-                    createAnchor.textContent = `${listName[i]}`
-                    createList.appendChild(createAnchor)
-                }
-
-                document.documentElement.scrollTop = 0;
-                document.body.scrollTop = 0;
-            })
-
+    if (e.target.classList.contains('home')) {
+        while (mainList.firstChild) {
+            mainList.removeChild(mainList.firstChild);
         }
-        //Cloned lists for desktops
-        const cloneList = createList.cloneNode(true)
-        mainList.appendChild(cloneList)
+        mainSection.style.cssText = 'display:block;'
+        nextContent.style.cssText = 'display:none;'
+        header.style.cssText = 'animation:opacity 2s forwards;'
+
+        for (let i = 0; i < listName.length; i++) {
+            const createList = document.createElement('li')
+            mainList.appendChild(createList)
+
+            const createAnchor = document.createElement('a')
+            createAnchor.href = `#${listId[i]}`
+            createAnchor.textContent = `${listName[i]}`
+            createList.appendChild(createAnchor)
+        }
+
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
     }
-}
+})
+
+hamburgerList.addEventListener('click', (e) => {
+    const listName = ['About', 'Projects', 'Contacts']
+    const listId = ['about', 'project', 'contact']
+
+    if (e.target.classList.contains('home')) {
+        while (hamburgerList.firstChild) {
+            hamburgerList.removeChild(hamburgerList.firstChild);
+        }
+        mainSection.style.cssText = 'display:block;'
+        nextContent.style.cssText = 'display:none;'
+        header.style.cssText = 'animation:opacity 2s forwards;'
+
+
+        for (let i = 0; i < listName.length; i++) {
+            const createList = document.createElement('li')
+            hamburgerList.appendChild(createList)
+
+            const createAnchor = document.createElement('a')
+            createAnchor.href = `#${listId[i]}`
+            createAnchor.textContent = `${listName[i]}`
+            createList.appendChild(createAnchor)
+        }
+
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+    }
+})
 
 //Reverse hamburger animation
 const reverseAnimation = () => {
